0853dfe4ea24745d0a862ec045dfeab46867d2e4
michellemoura <michellealvaresmoura@gmail.com>
2020-06-15 17:38:11 -0300

    nodeAPI v.0.1

---
+++ docs/mongodb.config.md
@@ -0,0 +1,15 @@
+##Install docker##
+
+##Command line##
+
+#Baixar img do mongo
+sudo docker pull mongo
+
+#Rodar mongo
+sudo docker run --name mongodb -p 27017:27017 -d mongo
+
+#status img docker exited (stoped)
+docker ps -a
+
+#start img docker
+docker start mongodb
---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const UserSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true
+  }, 
+  sobrenome: {
+    type: String,
+    required: true
+  },
+  logradouro: {
+    type: String,
+    required: true
+  },
+  cidade: {
+    type: String,
+    required: true
+  },
+  estado: {
+    type: String,
+    required: true
+  },
+  cep: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  senha: {
+    type: String,
+    required: true
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.plugin(mongoosePaginate);
+
+mongoose.model('User', UserSchema);
