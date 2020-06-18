b9c71cf9453237086dc35f30e836a4db55f27a78
michellemoura <michellealvaresmoura@gmail.com>
2020-06-18 16:14:19 -0300

    separando entidades

--- src/models/User.js
+++ src/models/User.js
@@ -10,33 +10,46 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  logradouro: {
-    type: String,
-    required: true
+  endereco: {
+    logradouro: {
+      type: String,
+      required: true
+    },
+    cidade: {
+      type: String,
+      required: true
+    },
+    estado: {
+      type: String,
+      required: true
+    },
+    cep: {
+      type: String,
+      required: true
+    },
   },
-  cidade: {
-    type: String,
-    required: true
+  conta: {
+    user: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    senha: {
+      type: String,
+      required: true
+    },
   },
-  estado: {
-    type: String,
-    required: true
-  },
-  cep: {
-    type: String,
-    required: true
-  },
-  user: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  senha: {
-    type: String,
-    required: true
+  instituicao: {
+    descricao: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+    }
   },
   createAt: {
     type: Date,
